22c21383725039709cc78cddd070937197f19e95
Esty212 <esty.dziubov@gmail.com>
2024-11-29 23:27:21 +0200

    implemented async task cancellation

--- Assets/Scripts/FileLoader.cs
+++ Assets/Scripts/FileLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,10 +12,10 @@ public class FileLoader : MonoBehaviour
     [SerializeField] private string fileName;
     [SerializeField][Range(0, 1)] private float loadPart;
 
-    public async Task FileLoaderMethod(Slider slider)
+    public async Task FileLoaderMethod(Slider slider, CancellationToken token)
     {
         Debug.Log("started loading file with name " +  fileName);
-        await Task.Delay(waitTime);
+        await Task.Delay(waitTime, token);
         slider.value += loadPart;
         Debug.Log("Ended loading file with name " + fileName);
     }
--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,18 +10,39 @@ public class LoadingScreen : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private List<FileLoader> fileloaders;
     [SerializeField] private Text text;
+    CancellationTokenSource cts = new CancellationTokenSource();
+    [SerializeField] private bool isPressed = false;
 
-    public async void RunFileLoaders()
+    private async void RunFileLoaders()
     {
         text.text = "Processing...";
         List<Task> tasks = new List<Task>();
         foreach (FileLoader loader in fileloaders)
         {
-            Task task = loader.FileLoaderMethod(slider);
+            Task task = loader.FileLoaderMethod(slider, cts.Token);
             tasks.Add(task);
         }
         await Task.WhenAll(tasks);
         text.text = "Done";
+    }
+
+    private void CancelToken()
+    {
+        cts.Cancel();
+        text.text = "Cancelled";
+    }
+
+    public void MyTest()
+    {
+        if (isPressed)
+        {
+            CancelToken();
+        }
+        else
+        {
+            isPressed = true;
+            RunFileLoaders();
+        }
     }
 
 
